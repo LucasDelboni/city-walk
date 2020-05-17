3ebd929d1f81a1d5d0f40b8a1eb3b034f687033c
Lucas Delboni <lucas.delboni2@gmail.com>
2020-05-16 22:36:54 -0300

    Updates

---
+++ precache-manifest.ebb4137637686cd7b6595f0507a20c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e404f758d69f49eeeb5202972617c2e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8ea0781ba836154f530",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "37d2d4aacc4b6b6b2e5b",
+    "url": "/static/js/2.8d9518fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d9518fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ea0781ba836154f530",
+    "url": "/static/js/main.9bad1724.chunk.js"
+  },
+  {
+    "revision": "8926be2c88de6066739f",
+    "url": "/static/js/runtime-main.2c37b1d4.js"
+  }
+]);
